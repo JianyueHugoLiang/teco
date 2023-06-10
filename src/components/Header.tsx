7739c5de70c865414980e60356796b5cc122a7e4
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-10 12:55:17 +0000

    2023.06.10-2155

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -26,7 +26,7 @@ export function Header() {
     <header className="bg-white">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
-          <a href="#" className="-m-1.5 p-1.5">
+          <a href="/" className="-m-1.5 p-1.5">
             <span className="sr-only">TecoStudio</span>
             <img className="h-8 w-auto" src="https://share.jianyuelab.cc/d/teco/logo/tecologo-nobg.svg" alt="" />
           </a>
@@ -80,7 +80,7 @@ export function Header() {
           </a>
         </Popover.Group>
         <div className="hidden lg:flex lg:flex-1 lg:justify-end">
-          <a href="#" className="text-sm font-semibold leading-6 text-gray-900">
+          <a href="https://bbs.tecostudio.cn/" className="text-sm font-semibold leading-6 text-gray-900">
             论坛 <span aria-hidden="true">&rarr;</span>
           </a>
         </div>
@@ -89,7 +89,7 @@ export function Header() {
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <a href="#" className="-m-1.5 p-1.5">
+            <a href="/" className="-m-1.5 p-1.5">
               <span className="sr-only">TecoStudio</span>
               <img
                 className="h-8 w-auto"
@@ -158,7 +158,7 @@ export function Header() {
               </div>
               <div className="py-6">
                 <a
-                  href="#"
+                  href="https://bbs.tecostudio.cn/"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-gray-900 hover:bg-gray-50"
                 >
                   论坛

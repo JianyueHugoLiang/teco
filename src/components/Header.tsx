d332ec7c5d43e28ce7ee13338ec541c5fa4898c4
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-10 12:35:43 +0000

    2023.06.10-2135

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -27,8 +27,8 @@ export function Header() {
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="#" className="-m-1.5 p-1.5">
-            <span className="sr-only">Your Company</span>
-            <img className="h-8 w-auto" src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600" alt="" />
+            <span className="sr-only">TecoStudio</span>
+            <img className="h-8 w-auto" src="https://share.jianyuelab.cc/d/teco/logo/tecologo-nobg.svg?color=indigo&shade=600" alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -96,7 +96,7 @@ export function Header() {
               <span className="sr-only">TecoStudio</span>
               <img
                 className="h-8 w-auto"
-                src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
+                src="https://share.jianyuelab.cc/d/teco/logo/tecologo-nobg.svg?color=indigo&shade=600"
                 alt=""
               />
             </a>

5b681b2a15bf38285c7c7219f52ae0ad24de8b05
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-12 08:21:11 +0000

    2023.06.12-1721

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,23 +1,69 @@
-import { getAllArticles } from "@/lib/getAllArticles";
-import Article from "@/components/Article";
+import Head from 'next/head'
 
-function ArticlesIndex({ articles }) {
+import { Card } from '@/components/Card'
+import { SimpleLayout } from '@/components/SimpleLayout'
+import { formatDate } from '@/lib/formatDate'
+import { getAllArticles } from '@/lib/getAllArticles'
+
+function Article({ article }) {
   return (
-    <div>
-      {articles.map((article) => (
-        <Article key={article.slug} article={article} />
-      ))}
-    </div>
-  );
+    <article className="md:grid md:grid-cols-4 md:items-baseline">
+      <Card className="md:col-span-3">
+        <Card.Title href={`/articles/${article.slug}`}>
+          {article.title}
+        </Card.Title>
+        <Card.Eyebrow
+          as="time"
+          dateTime={article.date}
+          className="md:hidden"
+          decorate
+        >
+          {formatDate(article.date)}
+        </Card.Eyebrow>
+        <Card.Description>{article.description}</Card.Description>
+        <Card.Cta>Read article</Card.Cta>
+      </Card>
+      <Card.Eyebrow
+        as="time"
+        dateTime={article.date}
+        className="mt-1 hidden md:block"
+      >
+        {formatDate(article.date)}
+      </Card.Eyebrow>
+    </article>
+  )
+}
+
+export default function ArticlesIndex({ articles }) {
+  return (
+    <>
+      <Head>
+        <title>Articles - JianyueHugo</title>
+        <meta
+          name="description"
+          content="All my thoughts and experience of programming, server development and translation"
+        />
+      </Head>
+      <SimpleLayout
+        title="Some Expression & Share"
+        intro="All my thoughts and experience of programming, server development and translation"
+      >
+        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+          <div className="flex max-w-3xl flex-col space-y-16">
+            {articles.map((article) => (
+              <Article key={article.slug} article={article} />
+            ))}
+          </div>
+        </div>
+      </SimpleLayout>
+    </>
+  )
 }
 
 export async function getStaticProps() {
-  const articles = await getAllArticles();
   return {
     props: {
-      articles,
+      articles: (await getAllArticles()).map(({ component, ...meta }) => meta),
     },
-  };
+  }
 }
-
-export default ArticlesIndex;

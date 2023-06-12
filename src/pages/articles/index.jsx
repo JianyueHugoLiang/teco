f372e8b7b08576c38d92b2bce7f8c7b16914b23a
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-12 05:03:59 +0000

    2023.06.12-1403

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -38,17 +38,13 @@ export default function ArticlesIndex({ articles }) {
   return (
     <>
       <Head>
-        <title>Articles - JianyueHugo</title>
-        <meta
-          name="description"
-          content="All my thoughts and experience of programming, server development and translation"
-        />
+        <title>新闻 - TecoStudio</title>
       </Head>
       <SimpleLayout
-        title="Some Expression & Share"
-        intro="All my thoughts and experience of programming, server development and translation"
+        title="新闻"
+        intro=""
       >
-        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="bg-white md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
           <div className="flex max-w-3xl flex-col space-y-16">
             {articles.map((article) => (
               <Article key={article.slug} article={article} />

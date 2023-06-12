02591e7af9ca1b1ff0947cc4bfb967ce1636a96b
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-12 05:09:35 +0000

    2023.06.12-1409

--- src/pages/articles/index.jsx
+++ src/pages/articles/index.jsx
@@ -1,7 +1,6 @@
 import Head from 'next/head'
 
 import { Card } from '@/components/Card.jsx'
-import { SimpleLayout } from '@/components/SimpleLayout'
 import { formatDate } from '@/lib/formatDate'
 import { getAllArticles } from '@/lib/getAllArticles'
 
@@ -40,15 +39,13 @@ export default function ArticlesIndex({ articles }) {
       <Head>
         <title>新闻 - TecoStudio</title>
       </Head>
-      <SimpleLayout>
-        <div className="md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
-          <div className="flex max-w-3xl flex-col space-y-16">
-            {articles.map((article) => (
-              <Article key={article.slug} article={article} />
-            ))}
-          </div>
+      <div className="bg-white md:border-l md:border-zinc-100 md:pl-6 md:dark:border-zinc-700/40">
+        <div className="flex max-w-3xl flex-col space-y-16">
+          {articles.map((article) => (
+            <Article key={article.slug} article={article} />
+          ))}
         </div>
-      </SimpleLayout>
+      </div>
     </>
   )
 }

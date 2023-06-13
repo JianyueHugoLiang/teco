d08d90139dd4db3501fc35ac91e470c31e15e439
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-13 04:04:28 +0000

    2023.06.13-1304

--- src/components/AboutForMain.tsx
+++ src/components/AboutForMain.tsx
@@ -2,7 +2,7 @@ export default function About() {
   return (
     <>
       <div className="bg-white">
-        <img src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-th.png" className="h-full w-full"></img>
+        <img src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-th.png" className="h-auto w-full"></img>
       </div>
     </>
   )

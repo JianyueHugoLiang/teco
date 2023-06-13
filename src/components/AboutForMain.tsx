fe8ff2ac3ff61cc0e812d3ed6d256fb0d6e06855
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-13 04:43:06 +0000

    2023.06.13-1443

--- src/components/AboutForMain.tsx
+++ src/components/AboutForMain.tsx
@@ -1,13 +1,11 @@
 export default function Example() {
   return (
-    <div className="bg-white sm:py-32">
-      <div className="relative isolate overflow-hidden bg-white">
-        <img
-          src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-me.png"
-          alt=""
-          className="h-full w-full"
-        />
-      </div>
+    <div className="relative isolate overflow-hidden bg-white">
+      <img
+        src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-me.png"
+        alt=""
+        className="h-full w-full"
+      />
     </div>
   )
 }

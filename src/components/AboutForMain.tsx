cea43038b977a04249ae75a7a018b1b4e6ade6fe
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-13 04:08:30 +0000

    2023.06.13-1308

--- src/components/AboutForMain.tsx
+++ src/components/AboutForMain.tsx
@@ -1,9 +1,11 @@
 export default function About() {
   return (
-    <>
-      <div className="bg-white">
-        <img src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-th.png" className="h-auto w-full"></img>
-      </div>
-    </>
+    <div className="relative isolate overflow-hidden bg-white py-24 sm:py-32">
+      <img
+        src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-th.png"
+        alt=""
+        className="absolute inset-0 -z-10 h-full w-full object-cover object-right md:object-center"
+      />
+    </div>
   )
 }

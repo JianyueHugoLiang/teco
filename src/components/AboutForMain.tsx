05ca8724b5e521963a067afda635f278eb569b3f
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-13 04:17:30 +0000

    2023.06.13-1317

--- src/components/AboutForMain.tsx
+++ src/components/AboutForMain.tsx
@@ -2,7 +2,7 @@ export default function Example() {
   return (
     <div className="relative isolate overflow-hidden bg-white">
       <img
-        src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-th.png"
+        src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-me.png"
         alt=""
         className="h-full w-full"
       />

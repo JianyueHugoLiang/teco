ec1f847a8c31077b3fd54fb50332c295e20dea89
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-13 04:14:58 +0000

    2023.06.13-1314

--- src/components/AboutForMain.tsx
+++ src/components/AboutForMain.tsx
@@ -1,10 +1,10 @@
 export default function Example() {
   return (
-    <div className="relative isolate overflow-hidden bg-white py-24 sm:py-32">
+    <div className="relative isolate overflow-hidden bg-white">
       <img
         src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-th.png"
         alt=""
-        className="absolute inset-0 -z-10 h-full w-full"
+        className="h-full w-full"
       />
     </div>
   )

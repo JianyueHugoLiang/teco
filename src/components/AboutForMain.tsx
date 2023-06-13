b8bc40c28a81b82c7c9885e21811342c432d10c7
Valor <122516876+FERRIJianyue@users.noreply.github.com>
2023-06-13 04:37:30 +0000

    2023.06.13-1437

--- src/components/AboutForMain.tsx
+++ src/components/AboutForMain.tsx
@@ -1,6 +1,6 @@
 export default function Example() {
   return (
-    <div className="bg-white py-24 sm:py-32">
+    <div className="bg-white sm:py-32">
       <div className="relative isolate overflow-hidden bg-white">
         <img
           src="https://photo.jianyuelab.cc/_data/i/upload/2023/06/12/20230612192916-50ebcf4d-me.png"
@@ -10,8 +10,8 @@ export default function Example() {
       </div>
       <div className="mx-auto max-w-7xl px-6 lg:px-8">
           <div className="mx-auto max-w-2xl lg:mx-0">
+            <h2 className="text-3xl font-bold tracking-tight text-gray-900 sm:text-4xl">TecoStudio 帝咕团队</h2>
               <p className="mt-6 text-lg leading-8 text-gray-600">
-              Valor 6/12/2023 7:10:37 PM
               「TecoStudio」，简称「TECO」 成立于 2020.2.22 ，是一个 Minecraft 综合小组。
               本小组开设有两个服务器，分别是 TecoCraft 内服 和 TecoEX 探索服 ，内服致力于探索 1.16.5 版本的红石技术，探索服致力于随版本更新并体验新特性。
               除此之外，TECO 小组是一个以游戏技巧（红石方向）和地图制作为主，综合服务器插件、联机教程和材质包等多个领域的综合性小组。
